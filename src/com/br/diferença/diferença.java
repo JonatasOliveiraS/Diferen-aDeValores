6760a0deebbabe53745547cd80c8d8976c27db1f
Jonatas Oliveira <Jonatas.sousa@zup.com.br>
2019-05-19 14:52:42 -0300

    adicionar programa de diferença de valores entre números

---
+++ src/com/br/diferença/diferença.java
@@ -0,0 +1,19 @@
+package com.br.diferença;
+import java.util.Scanner;
+public class diferença {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("informe o primeiro valor : ");
+		int númeroA = entrada.nextInt();
+		
+		System.out.println("informe o segundo valor : ");
+		int númeroB = entrada.nextInt();
+		
+		if (númeroA > númeroB);
+		System.out.println(" a diferença entre os números é de :" + (númeroA - númeroB));
+	}
+
+}
